234fcd55137f98ae90b606e00e529f45d32da9b6
Rick0202 <richard.harootunian@gmail.com>
2024-09-27 03:07:51 +0000

    added updates

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,15 +3,21 @@ import java.util.Scanner;
 public class Main {
   public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
-
-  System.out.println("This program will tell you if a letter you entered is a vowel or a consonant.  Enter a letter.\n");  
+    
+// This program will determine if a letter entered is a vowel or a consonant.
+    
     Scanner scanner = new Scanner(System.in);
     
     System.out.print("Enter a letter and the program will tell you if the letter is a vowel or a consonant: ");
     char ch = scanner.next().charAt(0);
-
+    
+// Establishing a vowel and consonant Strings so the letter being entered can be compared to the vowel and consonant Strings.
+    
     String vowels = "aeiouAEIOU";
     String consonant = "abcdfghjklmnpqrstvwxyzABCDFGHJKLMNPQRSTVWXYZ";
+    
+// Three if statements to determine if the letter is a vowel, consonant, or neither.
+    
     if (vowels.contains(String.valueOf(ch))) {
         System.out.println("\n" + ch + " is a vowel.");
     } else if (consonant.contains(String.valueOf(ch))){
